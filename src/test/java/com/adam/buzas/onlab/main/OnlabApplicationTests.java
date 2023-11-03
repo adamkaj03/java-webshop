3523fdbac979513db6066187a7c9c0f5e0693eec
adamkaj <adam2000buzas@gmail.com>
2023-11-03 19:49:27 +0100

    Egesz projekt

---
+++ src/test/java/com/adam/buzas/onlab/main/OnlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adam.buzas.onlab.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
